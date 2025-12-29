4fb670efbf76c5e3e8ce7c89b3e2168ec1628910
Jmoney67 <rael-litton.jericho@explore.academy>
2025-12-28 21:26:00 -0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -26,7 +26,7 @@ BOOT_SK_DIR="/usr/local/bin/bootmsg.sh"
 log "Installing Needed Things And Shit"
 mkdir -p "$MURK_DIR/plugins" "$MURK_DIR/pollen" || error "Failed To Installing Needed Things And Shit"
 touch "$CONF_DIV"
-curl -fsSL "$CONF_DIR" -o "$NONA_MOD_CONF" || error "Failed to download Config"
+curl -fsSL "$CONF_DIR" "$NONA_MOD_CONF" || error "Failed to download Config"
 
 log "Installing MushM"
 curl -fsSLo "$CROSH" "$MUSHM_URL" || error "Failed to download MushM"
